17565ca3af3b868f47d81161b8a0fdb36aa6b214
Leon <leon.kim.0509@gmail.com>
2017-10-13 20:02:41 -0400

    Mission Demolition
    Complete

---
+++ Assets/Scripts/Goal.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Goal : MonoBehaviour {
+	static public bool goalMet = false;
+	void OnTriggerEnter( Collider other){
+		if (other.gameObject.tag == "Projectile") {
+			Goal.goalMet = true;
+			Color c = GetComponent<Renderer>().material.color;
+			c.a = 1;
+			GetComponent<Renderer>().material.color = c;
+		}
+	}
+}
